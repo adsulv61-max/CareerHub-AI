6e7fcf3e35fc5ad0d34ee7ca8911ed4d6a21e0db
Vikrant Adsul <adsulv61@gmail.com>
2026-08-27 22:25:53 +0530

    Fix Django Render settings

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
         "BACKEND":
             "django.template.backends.django.DjangoTemplates",
 
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
 
         "APP_DIRS": True,
 
