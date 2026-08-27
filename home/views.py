bfe13d2ed66b05f923af4e5c7d4aa60636a57fde
Vikrant Adsul <adsulv61@gmail.com>
2026-08-27 23:48:02 +0530

    Fix os import for OpenAI

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from django.core.mail import send_mail
 from .models import ChatHistory, Application, Profile
 
 import requests
+import os
 from openai import OpenAI
 
 # =========================================================
