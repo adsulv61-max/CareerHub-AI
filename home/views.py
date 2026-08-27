1c82c810ef36b9324f04bc82782f3df933a2aa12
Vikrant Adsul <adsulv61@gmail.com>
2026-08-27 23:00:14 +0530

    Fix Django home template path

--- home/views.py
+++ home/views.py
@@ -14,7 +14,7 @@ import requests
 # =========================================================
 
 def home(request):
-    return render(request, "home/home.html")
+    return render(request, "home.html")
 
 
 # =========================================================
